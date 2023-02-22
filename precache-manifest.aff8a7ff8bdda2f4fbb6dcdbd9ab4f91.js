5113d6985b9b5a3e0fb381eb9eda1915b0deedb1
WildFire-27 <shreeya27naik@gmail.com>
2023-02-22 11:45:22 +0530

    Updates

---
+++ precache-manifest.aff8a7ff8bdda2f4fbb6dcdbd9ab4f91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e2ce5a4c3c0e4e03a194106c0e456a9",
+    "url": "/MovieDB/index.html"
+  },
+  {
+    "revision": "ddd46b29cd1109586716",
+    "url": "/MovieDB/static/css/main.ea5e0d82.chunk.css"
+  },
+  {
+    "revision": "55b1cd35439b6768ecd5",
+    "url": "/MovieDB/static/js/2.6f07566b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MovieDB/static/js/2.6f07566b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddd46b29cd1109586716",
+    "url": "/MovieDB/static/js/main.3f449b8a.chunk.js"
+  },
+  {
+    "revision": "851241c5a6d7cc9e6400",
+    "url": "/MovieDB/static/js/runtime-main.bf6f94ed.js"
+  }
+]);
